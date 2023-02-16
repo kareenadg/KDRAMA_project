6386a07ce0e23a822a845204fd801d5e5ff8ad35
kareenadg <kareenadg@gmail.com>
2023-02-16 15:32:39 +0100

    filters

---
+++ src/pages/Drama.jsx
@@ -0,0 +1,72 @@
+[
+    {
+      "title": "Goblin",
+      "year": "2016",
+      "image": "https://asianwiki.com/images/b/b2/Goblin-p04.jpg",
+      "description": "Goblin is a fantasy romance about a modern day goblin (Gong Yoo) who seeks to end his cursed immortal life and needs a human bride (Kim Go Eun) to do so. His life then becomes intertwined with a grim reaper (Lee Dong Wook) who is unable to remember his past.",
+      "id": "1"
+    },
+    {
+      "title": "Business Proposal",
+      "year": "2022",
+      "image": "https://upload.wikimedia.org/wikipedia/en/1/19/A_Business_Proposal.jpg",
+      "description": "It tells the story of Shin Ha-ri, an employee who accepts to go on a blind date in place of her friend, but finds out that her date is actually her boss.",
+      "id": "2"
+    },
+    {
+      "title": "Crash Landing On You",
+      "year": "2019",
+      "image": "https://upload.wikimedia.org/wikipedia/en/6/64/Crash_Landing_on_You_main_poster.jpg",
+      "description": "The romantic comedy drama follows a story of a beautiful heiress and a leading fashion businesswoman in South Korea meeting a North Korean military officer when she is swept up by a wind storm and accidentally getting crashed in North Korean territory while paragliding.",
+      "id": "3"
+    },
+    {
+      "title": "Extraordinary You",
+      "year": "2019",
+      "image": "https://upload.wikimedia.org/wikipedia/en/4/43/Extraordinary_You.jpg",
+      "description": "Extraordinary You is a fantasy romantic comedy about high school students (Kim Hye Yoon and Rowoon) who learn they are characters in a comic and decide to attempt to go against the plot and change their fates.",
+      "id": "4"
+    },
+    {
+      "title": "What's Wrong With Secretary Kim",
+      "year": "2018",
+      "image": "https://upload.wikimedia.org/wikipedia/en/1/10/What%27s_Wrong_with_Secretary_Kim.jpg",
+      "description": "Lee Young-joon (Park Seo-joon) is the vice-chairman of a major corporation. His world is shaken when, one day, his highly-capable secretary, Kim Mi-so (Park Min-young), announces that she will resign from her position after working for Lee Young-joon for nine years. Young-joon then decided to do whatever he can after talking to his best friend, who happens to be a board director in his company, to make sure Mi-so stays by his side.",
+      "id": "5"
+    },
+    {
+      "title": "Twenty-Five Twenty-One",
+      "year": "2022",
+      "image": "https://m.media-amazon.com/images/M/MV5BNWYzODM2NGEtOWUyZi00MmEyLWFmYmItZDI3NDYzMGI0NWI2XkEyXkFqcGdeQXVyNjc3MjQzNTI@._V1_FMjpg_UX1000_.jpg",
+      "description": "In 1998, Na Hee-do (Kim Tae-ri) is a member of the school fencing team at Seonjung Girls' High School, but due to the IMF crisis, the team is disbanded. To continue pursuing her passion, she transfers to Taeyang High School and later manages to become a member of the National Fencing Team.",
+      "id": "6"
+    },
+    {
+      "title": "It's Okay to Not Be Okay",
+      "year": "2020",
+      "image": "https://upload.wikimedia.org/wikipedia/en/5/58/It%27s_Okay_to_Not_Be_Okay_Poster.jpg",
+      "description": "An extraordinary road to emotional healing opens up for an selfish antisocial children's book writer and a selfless psych ward caretaker when they cross paths.",
+      "id": "7"
+    },
+    {
+      "title": "Love in the Moonlight",
+      "year": "2016",
+      "image": "https://upload.wikimedia.org/wikipedia/en/5/55/Love_in_the_Moonlight-official_poster.jpg",
+      "description": "The unlikely love story between a crown prince and his eunuch. Hong Ra On (Kim Yoo Jung) is a young woman during the Joseon Dynasty who disguises herself as a man named Sam Nom to support herself by ghostwriting and giving relationship advice to lovelorn men.",
+      "id": "8"
+    },
+    {
+      "title": "Descendants of the Sun",
+      "year": "2016",
+      "image": "https://upload.wikimedia.org/wikipedia/en/6/6e/DescendantsoftheSun.jpg",
+      "description": "A love story develops between Captain Yoo Shi Jin, from South Korean Special Forces and Doctor Kang Mo Yeon, who works as a Surgeon at Haesung Hospital in Seoul. They will find themselves in the middle of great events and deadly dangers, both in their Motherland and in the fictitious, war-torn country of Urk.",
+      "id": "9"
+    },
+    {
+      "title": "Extraordinary Attorney Woo",
+      "year": "2022",
+      "image": "https://upload.wikimedia.org/wikipedia/en/9/9d/Strange_Lawyer_Woo_Young-woo.png",
+      "description": "Extraordinary Attorney Woo tells the story of Woo Young-woo, a lawyer with autism who is raised by her single father. She grows up with one friend at school, Dong Geu-ra-mi, an oddball girl who protects her from school bullies. She graduates at the top of her law school class at Seoul National University.",
+      "id": "10"
+    }
+  ];
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -3,26 +3,53 @@ import "./Gallery.css";
 
 const Gallery = () => {
   const [dramas, setDramas] = useState([]);
-
-  const getDramas = async () => {
-    const res = await fetch(
-      `https://63ecf271be929df00cb5c6f7.mockapi.io/Kdramas`
-    );
-    const data = await res.json();
-    setDramas(data);
-  };
+  const [filteredDrama, setFilteredDrama] = useState([]);
 
   useEffect(() => {
-    getDramas();
+    fetch("https://63ecf271be929df00cb5c6f7.mockapi.io/Kdramas")
+      .then((res) => res.json())
+      .then((res) => {
+        setDramas(res);
+        setFilteredDrama(res);
+      });
   }, []);
+
+  const filterDrama = (keyword) => {
+    const filter = dramas.filter((drama) => {
+      drama.title.toLowerCase().includes(keyword.toLowerCase());
+      drama.year.includes(keyword);
+    });
+    setFilteredDrama(filter);
+  };
 
   return (
     <main>
-      {dramas.map((drama) => 
-        (<figure key={drama.id}>
+      <div className="filters">
+        <input type="text" onChange={(ev) => filterDrama(ev.target.value)} placeholder="Search..."/>
+        <button onClick={() => setFilteredDrama(dramas)}>ALL</button>
+        <button
+          onClick={() => {
+            const years = dramas.filter((drama) => drama.year < 2020);
+            setFilteredDrama(years);
+          }}
+        >
+          Before 2020
+        </button>
+        <button
+          onClick={() => {
+            const years = dramas.filter((drama) => drama.year >= 2020);
+            setFilteredDrama(years);
+          }}
+        >
+          After 2020
+        </button>
+      </div>
+      {filteredDrama.map((drama) => (
+        <figure key={drama.id}>
           <img src={drama.image} alt={drama.title} />
           <h3>{drama.title}</h3>
           <figcaption>{drama.year}</figcaption>
+          <button>＋</button>
         </figure>
       ))}
     </main>
